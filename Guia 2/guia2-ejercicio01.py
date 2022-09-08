253ed40ab8a9e1edf66f06bf4f1bb2c61812b2e7
Renzo <renzovarela0@gmail.com>
2022-09-08 20:23:04 -0300

    Realizado

--- Guia 2/guia2-ejercicio01.py
+++ Guia 2/guia2-ejercicio01.py
@@ -14,9 +14,11 @@ nombre = str()
 tiempo_carrera= float() #Tiempo en realizar la carrera
 tiempo_record = float() #Tiempo estimado
 
+
 #Salida
 ganador = str()
 promedio = float()
+minimo = float()
 
 #Limpiar/Inicializar Variables
 
@@ -26,19 +28,57 @@ nombre = ""
 tiempo_carrera= 0.0
 tiempo_record = 0.0
 
+
 #Salida
 ganador = ""
 promedio = 0.0
+minimo = 999.9
 
 #Asignar Valores a las Variables
 #Validación:
 while (competidores <= 0):
     competidores = int(input("Bienvenido a la finalisima ¿Cuantos competidores son?: "))
+    tiempo_record = float(input("Ingrese el tiempo record estimado: "))
     if (competidores <= 0):
         print("Error - no ha ingresado un valor correcto")
 
+#Usamos el bucle for para ejecutar la cantidad de competidores
 for competidores in range (1, competidores +1, 1):
+    tiempo_carrera= 0.0 #Limpiamos la variable
     nombre = str(input("¿Cual es tu nombre?: "))
-    tiempo_carrera = float(input("¿En cuanto tiempo finaliso la carrera?: "))
-    print("Competidor n°", competidores, nombre,"tu recorrido completo en", tiempo_carrera)
-   
+    tiempo_carrera = float(input("¿En cuanto tiempo finalizo la carrera?: "))
+    print("Competidor n°", competidores, nombre,"completaste la carrera en: ", tiempo_carrera)
+    print("") #Dejamos un espacio para mantener todo mas claro y oredenado
+
+
+    promedio = promedio + tiempo_carrera #Acumulamos la variable tiempo_carrera para luego sacar el promedio 
+
+
+    #Usamos esta condicional para guardar el valor minimo del recorrido completo en menor tiempo
+    if tiempo_carrera < minimo:
+        minimo = tiempo_carrera
+        ganador = nombre
+    
+
+promedio = promedio / competidores #Divido la variable acumulada del tiempo entre la cantidad de competidores
+
+print("El ganador de esta competición es: ", (ganador.upper()))
+print("Ha realizado la carrera en: ", minimo)
+print("El promedio entre todo los ciclistas es de: ", promedio)
+
+
+#                               P R U E B A  D E  E S C R I T O R I O 
+#| Entrada                                                    | Salida                                             |
+#| ---                                                        | ---                                                |
+#|Bienvenido a la finalisima ¿Cuantos competidores son?: 3    |Competidor n° 1 Renzo completaste la carrera en: 8  |
+#|Ingrese el tiempo record estimado: 10                       |Competidor n° 1 Ariel completaste la carrera en: 6  |
+#|                                                            |Competidor n° 1 Varela completaste la carrera en: 7 |
+#|¿Cual es tu nombre?:Renzo                                   |El ganador de esta competición es:  ARIEL           |
+#|En cuanto tiempo finalizo la carrera?: 8                    |Ha realizado la carrera en:  6.0                    |
+#|                                                            |El promedio entre todo los ciclistas es de:  7.0    |
+#|¿Cual es tu nombre?:Ariel                                   |                                                    |
+#|En cuanto tiempo finalizo la carrera?: 6                    |                                                    |
+#|                                                            |                                                    |
+#|¿Cual es tu nombre?:Varela                                  |                                                    |
+#|En cuanto tiempo finalizo la carrera?: 7                    |                                                    |
+
