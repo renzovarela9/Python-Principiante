8bd9bcffd491efd066018cf87f3b4b6e23a724f7
Renzo <renzovarela0@gmail.com>
2022-10-12 10:50:46 -0300

    Subido

--- Guia 2/guia2-ejercicio05.py
+++ Guia 2/guia2-ejercicio05.py
@@ -10,24 +10,25 @@
 usuario = str()
 subusuario = str()
 i = str()
+palabra_ta = str()
 incrementador = int()
 contador = int()
 contador2 = int()
 palabralarga = int()
 porcentaje = float()
+
 #Inicializar/limpiar Variables
 usuario = ""
 subusuario = ""
 i = "" #Iteracion
 incrementador = 0 #Cuenta y la posicion de cada letra
+palabra_ta = ""
 contador = 0
 contador2 = 0
 palabralarga = 0
 porcentaje = 0.0
 
 #Asignar Valores a las Variables
-
-
 usuario = input("¿Cual es tu nombre?: ")
 usuario = usuario.lower()
 usuario = usuario.strip(" ") # Borramos los espacios al inicio y final
@@ -36,27 +37,8 @@ for i in usuario:
     if i in "aeiouáéíóú":
         contador = contador+1
     
-    incrementador +=1 #Uso esta variable para compararla con el iterador, asi se en que lugar esta cada letra
-    print("iteracion", i, incrementador) 
-
-    #CONTADOR DE ESPACIOS
-    if i in " ":
-        print("el espacio esta la casilla: ", incrementador)
-        contador2 = contador2 +1 
-        
-        if i == " ":
-            subusuario = usuario[0 : incrementador]
-            
-            print("El recorte dice: ",subusuario)
-
-        #print("prueba sobre palabra mas larga",subusuario)
-    
-
-
-
 
 print("La cantidad de vocales usadas son: ", contador) #RESPUESTA A
-
 
 #PORCENTAJE DE VOCALES   #RESPUESTA B
 porcentaje = ( contador * 100)
@@ -66,6 +48,89 @@ print("El resultado de vocales usadas en la frase es de: ", porcentaje,"%")
 
 
 
-print("La cantidad de espacio que hay son: ", contador2)
+
+
+
+
+
+
+
+#Analisis de Texto.
+#El usuario ingresa una frase al comenzar el programa, la misma no puede tener longitud cero. 
+#La frase finaliza con un punto, y las palabras están separadas por espacios únicamente. Se debe mostrar:
+#a) Ver el porcentaje de vocales respecto del total de letras de la frase.
+#b) La longitud promedio de las palabras.
+#c) La longitud de la palabra mas larga del texto.
+#d) Cantidad de palabras que comienzan con "ta".
+
+#Declarar Variables
+usuario = str()
+subusuario = str()
+i = str()
+palabra_ta = str()
+incrementador = int()
+contador = int()
+contador2 = int()
+palabralarga = int()
+
+porcentaje = float()
+
+#Inicializar/limpiar Variables
+usuario = ""
+subusuario = ""
+i = "" #Iteracion
+incrementador = 0 #Cuenta y la posicion de cada letra
+palabra_ta = ""
+contador = 0
+contador2 = 0
+palabralarga = 0
+porcentaje = 0.0
+
+#Asignar Valores a las Variables
+
+usuario = input("¿Cual es tu nombre?: ")
+usuario = usuario.lower()
+usuario = usuario.strip(" ") # Borramos los espacios al inicio y final
+for i in  range (len(usuario)):
+    #CONTADOR DE VOCALES #RESPUESTA A
+    #if i in "aeiouáéíóú":
+     #   contador = contador+1
+    
+     #CONTADOR DE ESPACIOS
+    if i == " ":
+        print("el espacio esta la casilla: ", incrementador)
+        contador2 = contador2 +1 
+        
+    subusuario = usuario[0 : contador2]; 
+    usuario = usuario[contador2+1 : usuario.find(".")+1];  
+    #PALABRA MAS LARGA    #RESPUESTA C
+    if (len(subusuario) > palabralarga):
+        palabralarga = len(subusuario)
+
+
+
+
+#palabra_ta = usuario.find("ta")     #RESPUESTA D
+#for palabra_ta[0 : 2]  in "ta":
+ #   subusuario = palabra_ta 
+ #   contador2 = contador2 +1 
+     
+
+
+
+
+print("La cantidad de vocales usadas son: ", contador) #RESPUESTA A
+
+
+#PORCENTAJE DE VOCALES   #RESPUESTA B
+#porcentaje = ( contador * 100)
+#porcentaje = porcentaje / len(usuario) 
+#print("El resultado de vocales usadas en la frase es de: ", porcentaje,"%")
+
+#PALABRA MAS LARGA         #RESPUESTA C
+print("La palabra mas larga tiene:", palabralarga," Letras")
+#PALABRAS CON TA      #RESPUESTA D
+#print("Palabras que empienzan con 'ta' : ",contador2)
+
 
 
--- Guia 2/guia2-ejercicio07.py
+++ Guia 2/guia2-ejercicio07.py
@@ -2,65 +2,82 @@
 #Por cada función se conoce:cantidad de espectadores y descuento(S/N).
 #La carga termina cuando la cantidad de espectadores sea igual a 0(cero).El programa deberá:
 #Calcular la recaudación total del complejo,considerando que el valor de la entrada es de $50 en los días con descuento 
-#y $75 en los días sin descuento.Determinar cuántas funciones con descuentos efectuaron 
-#y qué porcentaje representan sobre el total de funciones
+#y $75 en los días sin descuento.
+#Determinar cuántas funciones con descuentos efectuaron y qué porcentaje representan sobre el total de funciones
 
 #Declarar Variables
 espectadores = int()
-sidescuento = int ()
 descuento = int()
+recaudacionS = int()
+recaudacionN = int()
+funcionS = int()
+funcionN = int()
 funcion = int()
+funcionGeneral = int()
+opcion = str()
 
 #Limpiar/Inicializar Variables
 espectadores = 0
-sidescuento = 0
 descuento = 0
-
+recaudacionS = 0
+recaudacionN = 0
+funcionS = 0
+funcionN = 0
+funcion = 0
+funcionGeneral = 0
+opcion = ""
 
 #Asignar Valores a las Variables
-
-print("Bienvenido a RECUCINES, ingrese la cantidad de entradas que desea comprar")
-print("Para finalizar el programa ingrese 0. ")
-
-
-
-
-espectadores = int(input("Ingrese la cantidad de entradas: "))
-
-#Validación
-while espectadores < 0:
+while espectadores >= 0:
+  espectadores= int(input("Ingrese la cantidad de espectadores (La carga termina cuando ingrese 0): "))
+  #Validación
+  while espectadores < 0: #No dejamos que el usuario ingrese numeros negativos
     if espectadores < 0:
-        print("Error- numero de entradas no validos")
-        espectadores = int(input("Ingrese la cantidad de entradas: "))
+        print("Error-Ingreso no valido")
+        espectadores= int(input("Ingrese la cantidad de espectadores (La carga termina cuando ingrese 0): "))
 
+  if espectadores > 0:
+    opcion = str(input("Ingrese 'Si' con descuento (Vale Martes y Juves) o 'No' sin descuento (Vale Lunes, Miercoles, Viernes y Domigo): "))
+    opcion = opcion.lower()
+    #Validación
+    while opcion != 'si' and opcion != 'no': #No dejamos que el usuario ingrese cualquier cosa
+      if opcion != 'si' and opcion != 'no':
+        print("Error-El valor que ingreso no corresponde")
+        opcion = str(input("Ingrese 'Si' con descuento (Vale Martes y Juves) o 'No' sin descuento (Vale Lunes, Miercoles, Viernes y Domigo): "))
+        opcion = opcion.lower()
 
+    if opcion == "si":
+      descuento = 50
+      recaudacionS = espectadores * descuento #Multiplicamos el precio de la entrada por el descuento
+      funcionS = funcionS +1 #Contador para luego sacar el porcentaje
   
-#Elije la pelicula          
-print("""1. 101 Dalmatas
-2. Toy Story: Live Action
-3. Gato con botas 2 
-4. Piratas del Caribe: Los ultimos tiempos
-5. ¿La vida tiene sentido?
-6. Sherk 5
-7. Avengers: The Return
-8. Titanic 2: Mi querido Jack
-9. Batman vs Superman 2
-10.No juguemos con fuego
-11.Los simpons 2""")
-  
+    elif opcion == "no":
+      descuento = 75
+      recaudacionN = espectadores * descuento #Multiplicamos el precio de la entrada por sin el descuento
+      funcionN = funcionN +1 #Contador para luego sacar el porcentaje
 
-print("Seleccione la función que quiere ver:")
-funcion = int(input())
+  #Fin del programa
+  if espectadores == 0:
+    break;
 
-#Realizamos operaciones segun la funcion que quiere ver
-if funcion == 1:
-  print("Usted a comprado ", espectadores, "entradas y le sale")
+  funcion = funcion +1 #Contador para luego sacar el porcentaje
 
-#Evalua si la variable espectador es igual a 0
-if (espectadores == 0):
-  print("")
-  print("Fin del programa")
-  print("")
-  
+funcionGeneral = ((funcionS *100) / funcion) #Usamos regla de 3 simples para el porcentaje
+
+print("Se hizo %",funcionGeneral,"Funciones con descuento")
 
 
+
+# P R U E B A  D E  E S C R I T O R I O 
+
+#|Entrada                |Salida                                                                                                          |
+#|espectadores: 7        |Ingrese la cantidad de espectadores (La carga termina cuando ingrese 0):                                        |                  
+#|opcion: si             |Ingrese 'Si' con descuento (Vale Martes y Juves) o 'No' sin descuento (Vale Lunes, Miercoles, Viernes y Domigo):|                                                                           
+#|                       |                                                                                                                |
+#|espectadores: 7        |Ingrese la cantidad de espectadores (La carga termina cuando ingrese 0):                                        |                                      
+#|opcion: no             |Ingrese 'Si' con descuento (Vale Martes y Juves) o 'No' sin descuento (Vale Lunes, Miercoles, Viernes y Domigo):|  
+#|                       |                                                                                                                |
+#|espectadores: 7        |Ingrese la cantidad de espectadores (La carga termina cuando ingrese 0):                                        |                                             
+#|opcion: no             |Ingrese 'Si' con descuento (Vale Martes y Juves) o 'No' sin descuento (Vale Lunes, Miercoles, Viernes y Domigo):|
+#|                       |                                                                                                                |
+#|espectadores: 0        |Se hizo % 33.333333333333336 Funciones con descuento                                                            |                                                                                                            
